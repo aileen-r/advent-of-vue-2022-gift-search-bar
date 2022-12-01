f17f5cbc0d75d91416deae72b0b4769ce49335e4
Aileen Rae <aileenlrae@gmail.com>
2022-12-01 23:19:53 +0000

    First pass without debounce

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <script setup>
-import { ref, watch } from 'vue'
+import { reactive, ref, watch } from 'vue';
 
-const searchTerm = ref('')
+const searchTerm = ref('');
+const products = ref([]);
 
-const findProducts = async term => {}
+const findProducts = async term => {
+  if (term) {
+  fetch(`https://dummyjson.com/products/search?q=${term}`)
+    .then(res => res.json())
+    .then(data => {
+      products.value = data.products
+    });
+  } else {
+    products.value = [];
+  }
+};
 
-watch(searchTerm, newTerm => findProducts(newTerm))
+watch(searchTerm, newTerm => findProducts(newTerm));
 </script>
 
 <template>
@@ -13,7 +24,7 @@ watch(searchTerm, newTerm => findProducts(newTerm))
     <h1 class="text-4xl font-bold">Gift Search Bar</h1>
     <input type="text" class="p-2 border-2 border-gray-dark" v-model="searchTerm" placeholder="Start typing..." />
     <ul class="list-disc">
-      <li>Display suggestions here</li>
+      <li v-for="product in products">{{product.title}}</li>
     </ul>
   </div>
 </template>
